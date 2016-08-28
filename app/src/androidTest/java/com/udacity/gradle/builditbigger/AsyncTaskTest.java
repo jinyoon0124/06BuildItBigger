8abf5fe9c43e005857f0dbf8db2dcb9390d3c068
Jin Yoon <jinyoon0124@gmail.com>
2016-08-28 04:56:49 -0400

    Test Added

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -0,0 +1,33 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Jin Yoon on 8/28/2016.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class AsyncTaskTest {
+
+    @Rule public final ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void asyncWorkToUpdateText(){
+        onView(withId(R.id.jokeButton)).perform(click());
+        onView(withId(R.id.joke_teller_activity_layout_textview)).check(matches(isDisplayed()));
+    }
+}
